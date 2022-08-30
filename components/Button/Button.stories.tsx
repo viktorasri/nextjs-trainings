207c5e0ff7a26c5afe7b0b20f66afa28dc5f44e8
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2022-08-30 22:41:29 +0300

    feat: add button component

---
+++ components/Button/Button.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Button } from './Button';
+
+export default {
+  title: 'Controls/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const BasicButton = Template.bind({});
+BasicButton.args = {
+  children: 'Button',
+};
