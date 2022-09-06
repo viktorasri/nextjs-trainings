08e37fe974bf7b5c13b29cd0227af8f65066ecb5
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2022-09-06 14:57:05 +0300

    add storybook interactions/jest-test

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,14 +1,19 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { expect } from '@storybook/jest';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { screen, userEvent } from '@storybook/testing-library';
 import { Button } from './Button';
 
 export default {
   title: 'Controls/Button',
   component: Button,
+  args: {
+    children: 'Button',
+  },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-export const BasicButton = Template.bind({});
-BasicButton.args = {
-  children: 'Button',
+export const PrimaryButton: ComponentStoryObj<typeof Button> = {
+  play: async ({ args }) => {
+    await userEvent.click(screen.getByRole('button'));
+    await expect(args.onClick).toHaveBeenCalled();
+  },
 };
