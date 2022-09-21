900b5b1e650d5663121975726e5a2ac0ad445d08
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2022-09-21 22:01:30 +0300

    add checkbox

---
+++ components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,16 @@
+import { expect } from '@storybook/jest';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { screen, userEvent } from '@storybook/testing-library';
+import { Checkbox } from './Checkbox';
+
+export default {
+  title: 'Controls/Checkbox',
+  component: Checkbox,
+} as ComponentMeta<typeof Checkbox>;
+
+export const BasicCheckbox: ComponentStoryObj<typeof Checkbox> = {
+  play: async ({ args }) => {
+    await userEvent.click(screen.getByText('✔'));
+    await expect(args.onChange).toHaveBeenCalled();
+  },
+};
